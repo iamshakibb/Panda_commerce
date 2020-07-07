43afd9c5f83661b9b51443e9e23e10912167fdfb
iamshakibb <shihabunshakib9346@gmail.com>
2020-07-07 06:43:22 +0600

    Update 0.5

---
+++ js/script.js
@@ -0,0 +1,17 @@
+
+$(document).ready(function(){
+    $('.banner_bg').slick({
+        prevArrow : '<i class="fas fa-chevron-left left_arrow"></i>',
+        nextArrow : '<i class="fas fa-chevron-right right_arrow"></i>',
+        dots: true,
+        dotsClass: 'banner_dots',
+    });
+
+    AOS.init({
+        delay: 0,
+        duration: 900,
+        mirror: true,
+    });
+  });
+
+
